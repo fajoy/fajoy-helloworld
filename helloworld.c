7251fa3c67bf9a9a2c0f439780e5c4242f050d7b
fajoy <wuminfajoy@gmail.com>
2011-09-23 14:42:19 +0800

    update hello

--- helloworld.c
+++ helloworld.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
  
-int main()
+int main(int argc,char *argv[],char *envp[])
 {
    printf("Hello World\n");
+   
+   int i=0;
+   for(i=0;i<argc;i++){
+	printf("arg[%d]=%s\n",i,argv[i]);
+   }
+   i=0;
+   while(envp[i]){
+	printf("envp[%d]=%s\n",i,envp[i]);
+	i++;
+   }
+
    return 0;
 }
 
 
-int test2()
-{
-return 2;
-}
